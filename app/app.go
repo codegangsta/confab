970cc86f0d8ee24756ae4a07873a6519dd6f5442
Jeremy Saenz <jeremy.saenz@gmail.com>
2013-10-26 13:29:01 -0700

    Initial commit

---
+++ app/app.go
@@ -0,0 +1,5 @@
+package app
+
+func Run(port int) {
+  println("hello worlds")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "github.com/codegangsta/confab/app"
+)
+
+
+func main() {
+  app.Run(3000)
+}
